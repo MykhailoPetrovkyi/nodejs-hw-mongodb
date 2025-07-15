219910853ec69ef1ef0512f7f1ddf2f973b33bb0
MykhailoPetrovkyi <petrovskij65@gmail.com>
2025-07-15 12:28:51 +0300

    init

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -13,7 +13,6 @@ export const initMongoConnection = async () => {
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
     );
-
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.log('Error while setting up mongo connection', error);
